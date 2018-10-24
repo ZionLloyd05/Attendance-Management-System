0b4bf700a1d09cd4f639de0575fc9b7a5958ebf2
ZionLloyd05 <zionlloyd05@gmail.com>
2018-10-24 13:00:31 +0100

    completed data access layer

---
+++ AMS.DAL/Domain_Objects/Domain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMS.DAL.Entities
+{
+    public partial class Lecturer : IEntity
+    {
+    }
+
+    public partial class Course : IEntity
+    {
+    }
+
+    public partial class Student : IEntity
+    {
+    }
+
+    public partial class Attendance : IEntity
+    {
+    }
+
+    public partial class StdAttendance : IEntity
+    {
+    }
+}
