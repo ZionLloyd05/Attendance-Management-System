64529d5ad4076032d62f4bba1df4360a8eae3473
ZionLloyd05 <zionlloyd05@gmail.com>
2018-10-24 12:44:47 +0100

    Building the data access layer

---
+++ AMS.DAL/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMS.DAL
+{
+    public class Entity:IEntity
+    {
+        public override string ToString()
+        {
+            return this.GetType().Name;
+        }
+    }
+}
