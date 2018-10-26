3fc724e3d765ac489e74e0dcc0e24d67840e6123
ZionLloyd05 <zionlloyd05@gmail.com>
2018-10-26 21:58:38 +0100

    Working on the repository layer

---
+++ AMS.Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AMS.DAL;
+
+namespace AMS.Repository
+{
+    interface IRepository
+    {
+        int Create(IEntity entity);
+        int Update(IEntity entity);
+        int Delete(IEntity entity);
+        IEntity GetOne(IEntity entity);
+        IEnumerable<IEntity> GetAll(IEntity entity);
+    }
+}
--- AMS.Repository/Repository.cs
+++ AMS.Repository/Repository.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AMS.DAL
+namespace AMS.Repository
 {
-    public interface IEntity
+    public class Repository
     {
     }
 }
